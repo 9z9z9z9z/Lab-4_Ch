9c03d901ff4a79504f56caf99f897f5305109a42
9z9z9z9z <rolyan293@gmail.com>
2022-12-08 19:54:45 +0300

    done

---
+++ Classes/ResearchTeam.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Lab_4
+{
+    public class ResearchTeam : Team, IEnumerable, INotifyPropertyChanged
+    {
+        private string theme; 
+        private TimeFrame timeLong;
+        private List<Person> persons;
+        private List<Paper> papers;
+        
+        public ResearchTeam() : this("Try programming", "Homyzhenko", TimeFrame.Long) { }
+        public ResearchTeam(string theme, string name, TimeFrame timeFrame)
+        {
+            this.theme = theme;
+            this.name = name;
+            this.timeLong = timeFrame;
+            this.persons = new List<Person>();
+            this.papers = new List<Paper>();
+        }
+        public string Theme
+        {
+            get { return theme; }
+            set { theme = value; }
+        }
+        public Team team
+        {
+            get
+            {
+                Team team = new Team(this.name, this.number);
+                return team;
+            }
+            set
+            {
+                this.name = value.Name;
+                this.number = value.Number;
+            }
+        }
+        public DateTime data
+        {
+            get
+            {
+                return papers.Max(obj => obj.date);
+            }
+        }
+        public TimeFrame GetTime()
+        {
+            return timeLong;
+        }
+        public Paper findPaper()
+        {
+            if (papers == null) return null;
+            Paper answer = new Paper();
+            foreach (Paper point in papers)
+            {
+                if (DateTime.Compare(point.date, answer.date) == 1)
+                {
+                    answer = point;
+                }
+            }
+            return answer;
+        }
+        public List<Paper> Papers() { return papers; }
+        public List<Person> Persons() { return persons; }
+        public void AddPersons(params Person[] items)
+        {
+            foreach (Person person in items)
+            {
+                persons.Add(person);
+            }
+        }
+        public bool this[TimeFrame frame]
+        {
+            get { return (frame == timeLong); }
+        }
+
+        public void AddPersons(List<Person> persons)
+        {
+            foreach (Person person in persons)
+            {
+                this.persons.Add(person);
+            }
+        }
+        public void AddPapers(List<Paper> papers)
+        {
+            foreach (Paper paper in papers)
+            {
+                this.papers.Add(paper);
+            }
+        }
+        public void addPapers(params Paper[] items)
+        {
+            foreach (Paper paper in items)
+            {
+                papers.Add(paper);
+            }
+        }
+        public override string ToString()
+        {
+            string list = "---------------------------------\n";
+            foreach (Paper paper in papers)
+            {
+                list += paper.ToString() + "---------------------------------\n";
+            }
+
+            return "Thheme:\t" + theme + "\nAuthor:\t" + name + "\nNumber:\t" + number
+                + '\n' + list + "=================================\n";
+        }
+        public string ToShortString()
+        {
+            return "Thheme:\t" + theme + "\nAuthor:\t" + name + "\nNumber:\t" + number
+                + "\n=================================\n";
+        }
+        public object DeepCopy()
+        {
+            ResearchTeam tmp = new ResearchTeam();
+            tmp.theme = theme;
+            tmp.name = name;
+            tmp.number = number;
+            tmp.timeLong = timeLong;
+            tmp.papers = papers;
+            return tmp;
+        }
+
+        public void sortByData()
+        {
+            papers.Sort();
+        }
+        public void sortByAuthor()
+        {
+            PaperComparerBySurname comparer = new PaperComparerBySurname();
+            papers.Sort(comparer);
+        }
+        public void sortByTitle()
+        {
+            IComparer<Paper> comparer = (IComparer<Paper>)new Paper();
+            papers.Sort(comparer);
+        }
+
+        IEnumerable<Person> GetEnumerator()
+        {
+            for (int i = 0; i < persons.Count; i++)
+            {
+                bool flag = false;
+                Person tmp_person = (Person)persons[i];
+                for (int j = 0; j < papers.Count; j++)
+                {
+                    Paper tmp_paper = (Paper)papers[j];
+                    if (tmp_person == tmp_paper.author) flag = true;
+                }
+                if (!flag) {
+                    yield return tmp_person; 
+                }
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return persons.GetEnumerator();
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void CirculationChanged(string value)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(value));
+        }
+        public void DateChanged(string value)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(value));
+        }
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.ComponentModel;
+
+namespace Lab_4
+{
+    public delegate KeyValuePair<TKey, TValue> GenerateElement<TKey, TValue>(int j);
+    public delegate TKey KeySelector<TKey>(ResearchTeam rt);
+    public delegate void ResearchTeamChangedHandler<TKey>(object source, ResearchTeamChangedEventArgs<TKey> args);
+
+    public class Program
+    {
+        
+        static public KeyValuePair<int, string> generator(int i)
+        {
+            string value = (i * i * i * i * i * i * i).ToString();
+            int key = i;
+            return new KeyValuePair<int, string>(key, value);
+        }
+
+        static public KeySelector<string> keySelector = delegate (ResearchTeam rt)
+        {
+            return rt.GetHashCode().ToString();
+        };
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            List<Person> persons1 = new List<Person>(2);
+            persons1.Add(new Person("Emil", "Markov", DateTime.Parse("14.11.2003")));
+            persons1.Add(new Person("Kolya", "Homyzhenko", DateTime.Parse("11.06.2003")));
+            List<Paper> paper1 = new List<Paper>(2);
+            paper1.Add(new Paper("Title1", new Person("Egor", "Teterchev", DateTime.Parse("10.08.2003")), DateTime.Parse("10.08.2003")));
+            paper1.Add(new Paper("Title2" ,new Person("Dima", "Panfilov", DateTime.Parse("09.07.2003")), DateTime.Parse("09.07.2003")));
+            ResearchTeam t1 = new ResearchTeam("MIET", "SPinteh", TimeFrame.Long);
+            t1.AddPersons(persons1);
+            t1.AddPapers(paper1);
+
+            List<Person> persons2 = new List<Person>(2);
+            persons1.Add(new Person(Inputs.RandomString(4), Inputs.RandomString(4), DateTime.Parse("14.11.2003")));
+            persons1.Add(new Person(Inputs.RandomString(4), Inputs.RandomString(4), DateTime.Parse("11.06.2003")));
+            List<Paper> paper2 = new List<Paper>(2);
+            paper1.Add(new Paper(Inputs.RandomString(4), new Person(Inputs.RandomString(4), Inputs.RandomString(4), DateTime.Parse("10.08.2003")), DateTime.Parse("10.08.2003")));
+            paper1.Add(new Paper(Inputs.RandomString(4), new Person(Inputs.RandomString(4), Inputs.RandomString(4), DateTime.Parse("09.07.2003")), DateTime.Parse("09.07.2003")));
+            ResearchTeam t2 = new ResearchTeam(Inputs.RandomString(4), Inputs.RandomString(4), TimeFrame.Long);
+            t2.AddPersons(persons2);
+            t2.AddPapers(paper2);
+            List<Person> persons3 = new List<Person>(2);
+            persons1.Add(new Person(Inputs.RandomString(4), Inputs.RandomString(4), DateTime.Parse("14.11.2003")));
+            persons1.Add(new Person(Inputs.RandomString(4), Inputs.RandomString(4), DateTime.Parse("11.06.2003")));
+            List<Paper> paper3 = new List<Paper>(2);
+            paper1.Add(new Paper(Inputs.RandomString(4), new Person(Inputs.RandomString(4), Inputs.RandomString(4), DateTime.Parse("10.08.2003")), DateTime.Parse("10.08.2003")));
+            paper1.Add(new Paper(Inputs.RandomString(4), new Person(Inputs.RandomString(4), Inputs.RandomString(4), DateTime.Parse("09.07.2003")), DateTime.Parse("09.07.2003")));
+            ResearchTeam t3 = new ResearchTeam(Inputs.RandomString(4), Inputs.RandomString(4), TimeFrame.Long);
+            t3.AddPersons(persons3);
+            t3.AddPapers(paper3);
+            List<Person> persons4 = new List<Person>(2);
+            persons1.Add(new Person(Inputs.RandomString(4), Inputs.RandomString(4), DateTime.Parse("14.11.2003")));
+            persons1.Add(new Person(Inputs.RandomString(4), Inputs.RandomString(4), DateTime.Parse("11.06.2003")));
+            List<Paper> paper4 = new List<Paper>(2);
+            paper1.Add(new Paper(Inputs.RandomString(4), new Person(Inputs.RandomString(4), Inputs.RandomString(4), DateTime.Parse("10.08.2003")), DateTime.Parse("10.08.2003")));
+            paper1.Add(new Paper(Inputs.RandomString(4), new Person(Inputs.RandomString(4), Inputs.RandomString(4), DateTime.Parse("09.07.2003")), DateTime.Parse("09.07.2003")));
+            ResearchTeam t4 = new ResearchTeam(Inputs.RandomString(4), Inputs.RandomString(4), TimeFrame.Long);
+            t4.AddPersons(persons4);
+            t4.AddPapers(paper4);
+
+            ResearchTeamCollection<string> rtc1 = new ResearchTeamCollection<string>(keySelector);
+            rtc1.AddDefaults(t1);
+            rtc1.AddDefaults(t2);
+            ResearchTeamCollection<string> rtc2 = new ResearchTeamCollection<string>(keySelector);
+            rtc2.AddDefaults(t3);
+            rtc2.AddDefaults(t4);
+
+            TeamsJournal tj = new TeamsJournal();
+            rtc1.reseachTeamChanged += tj.AddChanges;
+            rtc2.reseachTeamChanged += tj.AddChanges;
+
+            List<Person> persons5 = new List<Person>(2);
+            persons1.Add(new Person(Inputs.RandomString(4), Inputs.RandomString(4), DateTime.Parse("14.11.2003")));
+            persons1.Add(new Person(Inputs.RandomString(4), Inputs.RandomString(4), DateTime.Parse("11.06.2003")));
+            List<Paper> paper5 = new List<Paper>(2);
+            paper1.Add(new Paper(Inputs.RandomString(4), new Person(Inputs.RandomString(4), Inputs.RandomString(4), DateTime.Parse("10.08.2003")), DateTime.Parse("10.08.2003")));
+            paper1.Add(new Paper(Inputs.RandomString(4), new Person(Inputs.RandomString(4), Inputs.RandomString(4), DateTime.Parse("09.07.2003")), DateTime.Parse("09.07.2003")));
+            ResearchTeam t5 = new ResearchTeam(Inputs.RandomString(4), Inputs.RandomString(4), TimeFrame.Long);
+            t5.AddPersons(persons5);
+            t5.AddPapers(paper5);
+
+            // Add element
+            rtc1.AddDefaults(t5);
+            rtc2.AddDefaults(t5);
+            // Replace prop
+            t1.Number = 10;
+
+            // Replace elem
+            rtc1.Replace(t1, t5);
+            rtc2.Replace(t2, t5);
+
+            // Delete 
+            rtc1.Remove(t1);
+            t1.Name = "Emil";
+
+            // Change in replaced
+            t2.Number = 8;
+
+            Console.WriteLine(tj.ToString());
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
